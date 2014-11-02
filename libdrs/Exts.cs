713fca7394b918f7b09e4a951eb6096e1691805d
Taryn Hill <Phrohdoh@gmail.com>
2014-11-02 04:48:58 -0600

    Initial commit

---
+++ libdrs/Exts.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace libdrs
+{
+	public static class Exts
+	{
+		public static string F(this string fmt, params object[] args)
+		{
+			return string.Format(fmt, args);
+		}
+
+		public static string Reversed(this string s)
+		{
+			var arr = s.ToCharArray();
+			Array.Reverse(arr);
+			return new string(arr);
+		}
+
+		public static byte[] ReadBytes(this Stream s, int count)
+		{
+			if (count < 0)
+				throw new ArgumentOutOfRangeException("count", "Non-negative number required.");
+
+			var buf = new byte[count];
+			if (s.Read(buf, 0, count) < count)
+				throw new EndOfStreamException();
+
+			return buf;
+		}
+
+		public static int Peek(this Stream s)
+		{
+			var buf = new byte[1];
+			if (s.Read(buf, 0, 1) == 0)
+				return -1;
+
+			s.Seek(s.Position - 1, SeekOrigin.Begin);
+			return buf[0];
+		}
+
+		public static byte ReadUInt8(this Stream s)
+		{
+			return s.ReadBytes(1)[0];
+		}
+
+		public static ushort ReadUInt16(this Stream s)
+		{
+			return BitConverter.ToUInt16(s.ReadBytes(2), 0);
+		}
+
+		public static short ReadInt16(this Stream s)
+		{
+			return BitConverter.ToInt16(s.ReadBytes(2), 0);
+		}
+
+		public static uint ReadUInt32(this Stream s)
+		{
+			return BitConverter.ToUInt32(s.ReadBytes(4), 0);
+		}
+
+		public static int ReadInt32(this Stream s)
+		{
+			return BitConverter.ToInt32(s.ReadBytes(4), 0);
+		}
+
+		public static float ReadFloat(this Stream s)
+		{
+			return BitConverter.ToSingle(s.ReadBytes(4), 0);
+		}
+
+		public static double ReadDouble(this Stream s)
+		{
+			return BitConverter.ToDouble(s.ReadBytes(8), 0);
+		}
+
+		public static string ReadASCII(this Stream s, int length)
+		{
+			return new string(Encoding.ASCII.GetChars(s.ReadBytes(length)));
+		}
+
+		public static string ReadASCIIZ(this Stream s)
+		{
+			var bytes = new List<byte>();
+			var buf = new byte[1];
+
+			for (;;)
+			{
+				if (s.Read(buf, 0, 1) < 1)
+					throw new EndOfStreamException();
+
+				if (buf[0] == 0)
+					break;
+
+				bytes.Add(buf[0]);
+			}
+
+			return new string(Encoding.ASCII.GetChars(bytes.ToArray()));
+		}
+	}
+}
---
+++ libdrs/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+
+namespace libdrs
+{
+	class MainClass
+	{
+		static FileStream s;
+
+		public static void Main(string[] args)
+		{
+			if (args.Length < 1)
+				return;
+
+			s = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+
+			var copyright = s.ReadASCII(36);
+			s.ReadBytes(4); // \032.
+			Console.WriteLine("Copyright: {0}", copyright.Replace('\0', '0'));
+
+			var version = s.ReadASCII(4);
+			Console.WriteLine(version);
+
+			var tribe = s.ReadASCII(12);
+			Console.WriteLine("{0}", tribe.Replace('\0', '0'));
+
+			var numTables = s.ReadInt32();
+			Console.WriteLine("Number of tables: {0}", numTables);
+
+			var firstFileOffset = s.ReadInt32();
+
+			Console.WriteLine("Position should be 64: {0}", s.Position);
+
+			var tableOffsets = new int[4];
+			var filesInTable = new int[4];
+
+			// Starting to read table headers now
+			for (var i = 0; i < 4; i++)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Table {0}:", i + 1);
+
+				var filetype = s.ReadASCII(1);
+				if (!string.IsNullOrWhiteSpace(filetype))
+					Console.WriteLine("Filetype: {0}", filetype);
+
+				var rExt = s.ReadASCII(3);
+				Console.WriteLine("Extension: {0} ({1})", rExt, rExt.Reversed());
+
+				var tableOffset = s.ReadInt32();
+				Console.WriteLine("Table offset: {0}", tableOffset);
+				tableOffsets[i] = tableOffset;
+
+				var numberOfFiles = s.ReadInt32();
+				Console.WriteLine("Number of files: {0}", numberOfFiles);
+				filesInTable[i] = numberOfFiles;
+			}
+
+			for (var i = 0; i < 4; i++)
+			{
+				Console.WriteLine();
+
+				// Seek to the actual table
+				s.Position = (long)tableOffsets[i];
+
+				Console.WriteLine("Table {0}'s position: {1}", i + 1, s.Position);
+				Console.WriteLine("Files in table {0}: {1}", i + 1, filesInTable[i]);
+
+				// Read the actual files
+				for (var file = 0; file < filesInTable[i]; file++)
+				{
+					Console.WriteLine();
+
+					var fileID = BitConverter.ToInt32(s.ReadBytes(4), 0);
+					Console.WriteLine("\tFile ID: {0}", fileID);
+					
+					var fileOffset = s.ReadInt32();
+					Console.WriteLine("\tFile Offset: {0}", fileOffset);
+
+					var fileLength = s.ReadInt32();
+					Console.WriteLine("\tFile Length (bytes): {0}", fileLength);
+
+					Console.WriteLine("\tSeeking to {0}", fileOffset);
+					s.Position = (long)fileOffset;
+					File.WriteAllBytes(fileID.ToString(), s.ReadBytes(fileLength));
+
+					Console.WriteLine("\tWrote {0} ({1})", fileID, file);
+
+					Console.WriteLine("\tGoing to table header @ {0}", tableOffsets[i]);
+					s.Position = (long)tableOffsets[i];
+				}
+			}
+		}
+	}
+}
